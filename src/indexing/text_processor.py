4e28aa78c8bdd032e270904402721d6e554d3612
juliana schoener <juli.schoener@gmail.com>
2025-06-06 16:34:55 +0200

    This is the newest version of the project folder

---
+++ src/indexing/text_processor.py
@@ -0,0 +1,26 @@
+# src/indexing/text_processor.py
+
+# src/indexing/text_preprocessor.py
+
+import re
+
+class TextPreprocessor:
+    """Handles text preprocessing and tokenization"""
+
+    @staticmethod
+    def preprocess(text):
+        """Basic text preprocessing"""
+        if not text:
+            return []
+
+        text = text.lower()
+        text = re.sub(r'[^a-z0-9\s]', ' ', text)
+        words = text.split()
+        return [word for word in words if len(word) > 1]  # Remove single characters
+
+    @staticmethod
+    def preprocess_with_bigrams(text):
+        """Preprocessing with bigram support"""
+        words = TextPreprocessor.preprocess(text)
+        bigrams = [f"{words[i]}_{words[i + 1]}" for i in range(len(words) - 1)]
+        return words + bigrams
